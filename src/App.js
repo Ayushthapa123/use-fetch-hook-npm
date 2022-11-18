f7f5da83cf6ef95e6036830819fa0b4c5f961fc2
Ayush <thapaaayush115@gmail.com>
2022-11-18 11:55:18 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import useFetch from './hooks/use-fetch';
+
 function App() {
+
+  const { data, loading, error } = useFetch('https://api.publicapis.org/entries')
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <p>{loading}</p>}
+      {data && <p>Data is on the log</p>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
